0402ccb884555d2c13525441ba0b60295266d85c
Kennedy Zinn <kennedy.b.zinn@uth.tmc.edu>
2025-09-04 14:34:49 +0000

    changed 'end to end' argument to 'local' in bowtie2 line. More efficient; immproves alignment rate and speed.

--- scripts/bowtie_align.sh
+++ scripts/bowtie_align.sh
@@ -21,8 +21,11 @@ for folderNumber in {640..663}; do
 	echo "Trimming adapters of sample ${value} using CutAdapt."
 	cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o ${localData}/${value}.R1.trimmed.fastq.gz -p ${localData}/${value}.R2.trimmed.fastq.gz ${localData}/*_R1_*.fastq.gz ${localData}/*_R2_*.fastq.gz --minimum-length 20 --cores 4
 
+	#remove raw data (to keep memory volume under 105gb limit)
+	rm ${localData}/*KWA*
+
 	echo "Alignment of ${value} to mm39"
-	bowtie2 --end-to-end  --very-sensitive --no-mixed --no-discordant --phred33 -I 10 -X 700 -p 4  -x ${ref} -1 ${localData}/${value}.R1.trimmed.fastq.gz -2 ${localData}/${value}.R2.trimmed.fastq.gz -S /home/ssm-user/alignment/sam/${value}_bowtie2.sam &> /home/ssm-user/alignment/sam/bowtie2_summary/${value}_bowtie2.txt
+	bowtie2 --local  --very-sensitive --no-mixed --no-discordant --phred33 -I 10 -X 700 -p 4  -x ${ref} -1 ${localData}/${value}.R1.trimmed.fastq.gz -2 ${localData}/${value}.R2.trimmed.fastq.gz -S /home/ssm-user/alignment/sam/${value}_bowtie2.sam &> /home/ssm-user/alignment/sam/bowtie2_summary/${value}_bowtie2.txt
 
 	#upload trimmed data to s3 bucket
 	aws s3 cp "${localData}/${value}.R1.trimmed.fastq.gz" "${s3Path}${s3Folder}${s3NestFolder}"	
